4b1ed1d47cc307ef7f048e737edcc3f2999e240d
joseph <zhao.joseph@yahoo.ca>
2022-06-20 00:03:00 -0400

    feat:  added rate limiter using express-rate-limit npm module

--- config/index.js
+++ config/index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 const mysql  = require('mysql');
 const jwt = require('jsonwebtoken');
+const rateLimit = require('express-rate-limit');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -37,6 +38,15 @@ if(process.env.NODE_ENV == "production"){
         multipleStatements: true,
     })  
 }   
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+app.use(limiter);
 
 const {errorHandler} = require('../middleware/errorHandler');
 const runQuery = require('../dbFuncs/query')(pool);
